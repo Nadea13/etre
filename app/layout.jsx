33a725f3d98b2257a27a53ebba88715580d065ec
Nadea13 <nathanon.idea@gmail.com>
2025-09-24 00:43:47 +0700

    design web

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import { Gemunu_Libre } from "next/font/google"
+import "./globals.css"
+
+const gemunuLibre = Gemunu_Libre({
+    variable: "--font-gemunu-libre",
+    subsets: ["latin"]
+})
+
+export const metadata = {
+    title: "ETRE",
+    description: "",
+}
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body className={`${gemunuLibre.className} antialiased`}>
+                {children}
+            </body>
+        </html>
+    )
+}
